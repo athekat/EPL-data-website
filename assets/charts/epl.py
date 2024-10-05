988564750ec68398d13ea46c252bacdc5d2911a8
Alan <alanc90@gmail.com>
2024-10-05 18:48:28 -0300

    new design

--- assets/charts/epl.py
+++ assets/charts/epl.py
@@ -4,30 +4,34 @@ from io import BytesIO
 import zipfile
 import os
 import plotly.graph_objects as go
+import roman
 
-def download_and_unzip(url, output_dir):
-    # Create the output directory if it doesn't exist
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
 
-    # Download the zip file
-    response = requests.get(url)
-    zip_file = zipfile.ZipFile(BytesIO(response.content))
+datoslibros = 'downloaded_files/epublibre.csv'
 
-    # Extract the contents of the zip file
-    zip_file.extractall(output_dir)
+# def download_and_unzip(url, output_dir):
+#     # Create the output directory if it doesn't exist
+#     if not os.path.exists(output_dir):
+#         os.makedirs(output_dir)
 
-    # Get the list of extracted files
-    extracted_files = zip_file.namelist()
+#     # Download the zip file
+#     response = requests.get(url)
+#     zip_file = zipfile.ZipFile(BytesIO(response.content))
 
-    # Close the zip file
-    zip_file.close()
+#     # Extract the contents of the zip file
+#     zip_file.extractall(output_dir)
 
-    # Return the list of extracted files
-    return extracted_files
+#     # Get the list of extracted files
+#     extracted_files = zip_file.namelist()
+
+#     # Close the zip file
+#     zip_file.close()
+
+#     # Return the list of extracted files
+#     return extracted_files
 
 def publication_trend_line_chart(file_path):
-    year_counts = {}
+    century_counts = {}
     with open(file_path, 'r', newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
@@ -35,20 +39,18 @@ def publication_trend_line_chart(file_path):
             if year_str != 'N/A':
                 try:
                     year = int(year_str[:4])  # Extract first four characters and convert to integer
-                    if year in year_counts:
-                        year_counts[year] += 1
+                    century = (year + 99) // 100  # Corrected century calculation
+                    if century in century_counts:
+                        century_counts[century] += 1
                     else:
-                        year_counts[year] = 1
+                        century_counts[century] = 1
                 except ValueError:
                     print(f"Ignoring invalid year value: {year_str}")
-    
-    # Sort the year_counts by year
-    sorted_year_counts = dict(sorted(year_counts.items()))
 
     # Create Plotly line chart
-    fig = go.Figure(data=go.Scatter(x=list(sorted_year_counts.keys()), y=list(sorted_year_counts.values()), mode='lines'))
-    fig.update_layout(title='Libros publicados por año',
-                      xaxis_title='Año',
+    fig = go.Figure(data=[go.Bar(x=list(century_counts.keys()), y=list(century_counts.values()))])
+    fig.update_layout(title='',
+                      xaxis_title='Siglo',  # Change x-axis title to "Siglo"
                       yaxis_title='Cantidad de libros')
     #fig.show()
     fig.write_html("publicacion.html")
@@ -79,7 +81,7 @@ def genre_distribution_bar_chart(file_path):
     
     # Create Plotly bar chart
     fig = go.Figure(data=[go.Bar(x=list(genre_counts.keys()), y=list(genre_counts.values()))])
-    fig.update_layout(title='Cantidad de libros por género',
+    fig.update_layout(title='',
                       xaxis_title='Género',
                       yaxis_title='Cantidad de libros')
     fig.write_html("genero.html")
@@ -99,15 +101,15 @@ def language_distribution_pie_chart(file_path):
     # Create Plotly pie chart
     fig = go.Figure(data=[go.Pie(labels=list(language_counts.keys()), values=list(language_counts.values()))])
     fig.update_traces(textposition='inside', textinfo='label+percent', insidetextfont=dict(color='#FFFFFF'))
-    fig.update_layout(title='Cantidad de libros por idioma')
+    fig.update_layout(title='')
     fig.write_html("idiomas.html")
     #fig.show()
 
 # Example usage
-download_url = 'https://rss.epublibre.org/csv'
-output_directory = './downloaded_files'
-extracted_files = download_and_unzip(download_url, output_directory)
-csv_file_path = os.path.join(output_directory, extracted_files[0])
-publication_trend_line_chart(csv_file_path)
-genre_distribution_bar_chart(csv_file_path)
-language_distribution_pie_chart(csv_file_path)
+# download_url = 'https://rss.epublibre.org/csv'
+# output_directory = './downloaded_files'
+# extracted_files = download_and_unzip(download_url, output_directory)
+# csv_file_path = os.path.join(output_directory, extracted_files[0])
+publication_trend_line_chart(datoslibros)
+genre_distribution_bar_chart(datoslibros)
+language_distribution_pie_chart(datoslibros)
